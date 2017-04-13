e8bb849c84064166976282c1b8eedb203a0450f6
Allison Northrop <agnorthrop@gmail.com>
2017-04-13 09:32:07 -0700

    turning in

--- whiteboardpractice.rb
+++ whiteboardpractice.rb
@@ -57,4 +57,45 @@ def stretch(array)
   print stretched_array
 end
 
-stretch([18, 7, 4, 24, 11])
+
+# Write a method named numUnique that accepts a sorted array of integers as a parameter and utilizes a hash to calculate and return the number of unique values in the array. The array is guaranteed to be in sorted order, which means that duplicates will be grouped together.
+#
+# For example, if a variable called list stores the following values: list = [5, 7, 7, 7, 8, 22, 22, 23, 31, 35, 35, 40, 40, 40, 41] then the call of numUnique(list) should return 9 because this list has 9 unique values (5, 7, 8, 22, 23, 31, 35, 40 and 41).
+#
+# It is possible that the list might not have any duplicates. For example if list instead stored this sequence of values: list = [1, 2, 11, 17, 19, 20, 23, 24, 25, 26, 31, 34, 37, 40, 41] Then a call on the method would return 15 because this list contains 15 different values.
+#
+# If passed an empty list, your method should return 0.
+
+
+
+def numUnique(array)
+  #utilizes a hash to return a number of unique vallues in tha array
+  #just make an array into a hash because hashes deal with duplicate keys as just making them into one.
+  hash = {}
+  array.each do |num|
+    num == num.key
+    hash << num
+  end
+  print hash
+end
+
+numUnique([5, 7, 7, 7, 8, 22, 22, 23, 31, 35, 35, 40, 40, 40, 41])
+
+
+
+# Write a method named numUnique2 that functions the same as numUnique except that it does NOT use a hash to solve the problem. In this version, you should only iterate over the array, but use no auxiliary storage outside of fixnum variables, if needed.
+#
+# Remember that you can assume that the values in the array appear in sorted (nondecreasing) order.
+
+
+
+
+
+
+
+
+
+
+
+
+#TODO
